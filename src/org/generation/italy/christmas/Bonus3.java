48805a31d56320b91d1fa0cef941745881f818ea
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-23 15:39:47 +0100

    finisch bonus3 with map collection

--- src/org/generation/italy/christmas/Bonus3.java
+++ src/org/generation/italy/christmas/Bonus3.java
@@ -1,7 +1,30 @@
 package org.generation.italy.christmas;
 
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+
 public class Bonus3 {
 	public static void main(String[] args) {
-		
+		//apriamo un scanner per la comunicazione
+				Scanner sc = new Scanner(System.in);
+				
+				// creo la collection dove inseriro i caratteri
+				Map<Integer, Character> charat = new HashMap<>();
+				
+				
+				
+				System.out.println("Inserisci una parola");
+				String word = sc.nextLine();
+				//ciclo sulle parole per trovare il numero di vocali contenute
+				for (int i = 0; i < word.length(); i++) {
+					//prendo il sinoglo carattere e lo aggingo collection
+					char wordChar = word.charAt(i);
+					charat.put(i, wordChar);
+				}
+				
+				System.out.println(charat);
+				// chiudiamo il canale di comunicazione
+				sc.close();
 	}
 }
