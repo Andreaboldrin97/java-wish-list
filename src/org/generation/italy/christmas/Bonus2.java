04d77b40649d56ce4c8c36aa8370d7dbf3082fa2
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-23 15:34:18 +0100

    finisch bonus2 with set collection

--- src/org/generation/italy/christmas/Bonus2.java
+++ src/org/generation/italy/christmas/Bonus2.java
@@ -1,8 +1,32 @@
 package org.generation.italy.christmas;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Bonus2 {
 
 	public static void main(String[] args) {
 		
+		//apriamo un scanner per la comunicazione
+		Scanner sc = new Scanner(System.in);
+		
+		// creo la collection dove inseriro i caratteri
+		Set<Character> charat = new HashSet<>();
+		
+		
+		
+		System.out.println("Inserisci una parola");
+		String word = sc.nextLine();
+		//ciclo sulle parole per trovare il numero di vocali contenute
+		for (int i = 0; i < word.length(); i++) {
+			//prendo il sinoglo carattere e lo aggingo collection
+			char wordChar = word.charAt(i);
+			charat.add(wordChar);
+		}
+		
+		System.out.println(charat);
+		// chiudiamo il canale di comunicazione
+		sc.close();
 	}
 }
